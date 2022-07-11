bdb6b24a3f7dee77a195905569dc8ae50785f08b
aliya <kolibri.alia@yandex.ru>
2022-07-12 01:29:00 +0300

    sdxfbdHBKBKljulf

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ api.add_resource(constellations_resource.CatalogListResource, "/api/get/cons")
 
 
 def main():
-    db_session.global_init("db/hestia_main.db")
+    db_session.global_init("db/kringe.db")
 
 
 @app.route("/base")
