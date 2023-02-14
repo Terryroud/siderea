19e9ea830279601113b79b06269b6cb35232601f
aliya <kolibri.alia@yandex.ru>
2023-02-14 16:34:54 +0300

    sdfgh

--- main.py
+++ main.py
@@ -183,4 +183,4 @@ app.add_url_rule('/learn/<int:id>', view_func=learn, methods=['GET', 'POST'])
 
 if __name__ == "__main__":
     main()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="localhost", port=int(os.environ.get("PORT", 8080)))
